83378ad3e2d12464d528648a16791abf32db765b
Matt McElwee <mmcelwee@dmsi.com>
2018-04-06 18:49:34 -0500

    Corrected an assignment notification issue - Added subscription to assignment notification http call to get it to fire - Added response checks in profile.ts to stop console errors - Added a date formatter to the profile request cards

--- src/app/assign-person/assign-person.component.ts
+++ src/app/assign-person/assign-person.component.ts
@@ -10,56 +10,61 @@ import * as moment from 'moment';
 })
 export class AssignPersonComponent implements OnInit {
 
-	firebase = window['firebase'];
-	sub: any;
+  firebase = window['firebase'];
+  sub: any;
 
   constructor(private http: Http, private router: ActivatedRoute, private nav: Router) {}
 
   ngOnInit() {
-		this.sub = this.router.params.subscribe((params) => {
-			const personId = params['id'];
-			const coachId = params['coachId'];
-			this.firebase.database()
-				.ref(`people/${personId}`)
-				.once('value')
-				.then(res => res.val())
-				.then(person => {
-					return this.firebase.database()
-						.ref(`people/${coachId}`)
-						.once('value')
-						.then(res => ({person, coach: res.val()}))
-				})
-				.then(({person, coach}) => {
-					this.firebase.database()
-						.ref(`requests/${personId}`)
-						.remove();
-					const assignmentData = {
-						personId: personId,
-						firstName: person.firstName,
-						lastName: person.lastName,
-						image: person.image || '',
-						coach: {
-							id: coachId,
-							firstName: coach.firstName,
-							lastName: coach.lastName,
-							image: coach.image || '',
-							email: coach.email
-						},
-						status: 'PENDING',
-						assigned: moment().unix()
-					};
-					this.http.post('https://api.flatlandchurch.com/v2/email/coachingAssignments', {
-						person,
-						coach
-					});
-					return this.firebase.database()
-						.ref(`assignments/${moment().unix()}`)
-						.set(assignmentData);
-				})
-				.then(() => {
-					this.nav.navigate(['/person/', personId]);
-				});
-		});
+    this.sub = this.router.params.subscribe((params) => {
+      const personId = params['id'];
+      const coachId = params['coachId'];
+      this.firebase.database()
+        .ref(`people/${personId}`)
+        .once('value')
+        .then(res => res.val())
+        .then(person => {
+          return this.firebase.database()
+            .ref(`people/${coachId}`)
+            .once('value')
+            .then(res => ({person, coach: res.val()}))
+        })
+        .then(({person, coach}) => {
+          this.firebase.database()
+            .ref(`requests/${personId}`)
+            .remove();
+          const assignmentData = {
+            personId: personId,
+            firstName: person.firstName,
+            lastName: person.lastName,
+            image: person.image || '',
+            coach: {
+              id: coachId,
+              firstName: coach.firstName,
+              lastName: coach.lastName,
+              image: coach.image || '',
+              email: coach.email
+            },
+            status: 'PENDING',
+            assigned: moment().unix()
+          };
+          console.log({ person, coach });
+          this.http.post('https://api.flatlandchurch.com/v2/emails/coachingAssignments?key=202f1c42-7054-46ee-8ca2-ddc85f9c789b', {
+            person,
+            coach
+          }).subscribe(res => {
+            // Apppppparrently observers only work if you're subscribed? Idk, I could look it up but I won't
+            console.log('Notification sent!');
+          });
+          console.log(assignmentData);
+          return this.firebase.database()
+            .ref(`assignments/${moment().unix()}`)
+            .set(assignmentData);
+        })
+        .then(() => {
+          this.nav.navigate(['/person/', personId]);
+        });
+    });
   }
 
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { ActivatedRoute } from '@angular/router';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-profile',
@@ -9,57 +10,80 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
 
-	firebase = window['firebase'];
-	sub: any;
-	personId: string;
-	person: object;
-	request = {};
-	assignmentsPBC = [];
-	coachingAssignments = [];
+  firebase = window['firebase'];
+  sub: any;
+  personId: string;
+  person: object;
+  request;
+  assignmentsPBC = [];
+  coachingAssignments = [];
 
-	constructor(private http: Http, private router: ActivatedRoute) {}
+  constructor(private http: Http, private router: ActivatedRoute) {}
 
-	ngOnInit() {
-		this.sub = this.router.params.subscribe((params) => {
-			this.personId = params['id'];
-			this.firebase.database().ref(`people/${this.personId}`)
-				.on('value', res => {
-					this.person = res.val();
-				});
-			this.firebase.database()
-				.ref(`requests/${this.personId}`)
-				.on('value', res => {
-					this.request = res.val();
-				});
-			this.firebase.database()
-				.ref(`assignments`)
-				.orderByChild('personId')
-				.equalTo(this.personId)
-				.on('value', res => {
-					const data = res.val();
-					this.assignmentsPBC = Object.keys(data).map(key => {
-						return data[key];
-					});
-				});
-				this.firebase.database()
-					.ref('assignments')
-					.orderByChild('coach/id')
-					.equalTo(this.personId)
-					.on('value', res => {
-						const data = res.val();
-						this.coachingAssignments = Object.keys(data).map(key => {
-							return data[key];
-						});
-					})
-		});
-	}
+  ngOnInit() {
+    this.sub = this.router.params.subscribe((params) => {
+      this.personId = params['id'];
+      this.firebase.database().ref(`people/${this.personId}`)
+        .on('value', res => {
+          this.person = res.val();
+        });
+      this.firebase.database()
+        .ref(`requests/${this.personId}`)
+        .on('value', res => {
+          if (res.val()) {
+            this.request = this.formatTimes(res.val());
+          }
+        });
+      this.firebase.database()
+        .ref(`assignments`)
+        .orderByChild('personId')
+        .equalTo(this.personId)
+        .on('value', res => {
+          if (res.val()) {
+            const data = res.val();
+            this.assignmentsPBC = Object.keys(data)
+              .map(key => {
+                return data[key];
+              })
+              .map(this.formatTimes);
+          }
+        });
+      this.firebase.database()
+        .ref('assignments')
+        .orderByChild('coach/id')
+        .equalTo(this.personId)
+        .on('value', res => {
+          if (res.val()) {
+            const data = res.val();
+            this.coachingAssignments = Object.keys(data)
+              .map(key => {
+                return data[key];
+              })
+              .map(this.formatTimes);
+          }
+        })
+    });
+  }
 
-	callPhone = (phoneNumber) => {
-		window.location.href = `tel:${phoneNumber}`;
-	};
+  callPhone = (phoneNumber) => {
+    window.location.href = `tel:${phoneNumber}`;
+  };
 
-	openEmail = (emailAddress) => {
-		window.location.href = `mailto:${emailAddress}`;
-	};
+  openEmail = (emailAddress) => {
+    window.location.href = `mailto:${emailAddress}`;
+  };
+
+  formatTimes = data => {
+    if (data['started']) {
+      data['started'] = moment.unix(data['started']).format('MMMM d, YYYY');
+    }
+    if (data['completed']) {
+      data['completed'] = moment.unix(data['completed']).format('MMMM d, YYYY');
+    }
+    if (data['assigned']) {
+      data['assigned'] = moment.unix(data['assigned']).format('MMMM d, YYYY');
+    }
+    return data;
+  }
 
 }
