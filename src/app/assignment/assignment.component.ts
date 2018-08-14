8604b36117fdc16a01d0525eb0d0fa7b576b790b
Matt McElwee <mmcelwee@dmsi.com>
2018-08-14 06:59:30 -0500

    Corrected an issue where assignments were disappearing

--- src/app/assignment/assignment.component.ts
+++ src/app/assignment/assignment.component.ts
@@ -9,61 +9,61 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AssignmentComponent implements OnInit {
 
-	firebase = window['firebase'];
-	sub: any;
-	person = {};
-	coaches = [];
-	personId: string;
+  firebase = window['firebase'];
+  sub: any;
+  person = {};
+  coaches = [];
+  personId: string;
 
   constructor(private http: Http, private router: ActivatedRoute) {}
 
   ngOnInit() {
-		this.sub = this.router.params.subscribe((params) => {
-			this.personId = params['id'];
-			this.firebase.database().ref(`people/${this.personId}`)
-				.on('value', res => {
-					this.person = res.val();
-				});
-		});
+    this.sub = this.router.params.subscribe((params) => {
+      this.personId = params['id'];
+      this.firebase.database().ref(`people/${this.personId}`)
+        .on('value', res => {
+          this.person = res.val();
+        });
+    });
 
-		this.firebase.database()
-			.ref('people')
-			.orderByChild('isCoach')
-			.equalTo(true)
-			.on('value', res => {
-				const data = res.val();
-				this.coaches = Object.keys(data).map(key => {
-					const coach = data[key];
-					coach.id = key;
-					coach.type = 'COACH';
-					coach.pbcId = this.personId;
-					return coach;
-				});
-			});
+    this.firebase.database()
+      .ref('people')
+      .orderByChild('isCoach')
+      .equalTo(true)
+      .on('value', res => {
+        const data = res.val();
+        this.coaches = Object.keys(data).map(key => {
+          const coach = data[key];
+          coach.id = key;
+          coach.type = 'COACH';
+          coach.pbcId = this.personId;
+          return coach;
+        });
+      });
   }
 
-	searchCoaches(searchTerm: HTMLInputElement) {
-		if (searchTerm.value === '') {
-			this.firebase.database()
-				.ref('people')
-				.orderByChild('isCoach')
-				.equalTo(true)
-				.on('value', res => {
-					const data = res.val();
-					this.coaches = Object.keys(data).map(key => {
-						const coach = data[key];
-						coach.id = key;
-						coach.type = 'COACH';
-						coach.pbcId = this.personId;
-						return coach;
-					});
-				});
-		}
-		this.coaches = this.coaches.filter(coach => {
-			if (coach.firstName.includes(searchTerm.value)) {
-				return coach;
-			}
-		});
-	}
+  searchCoaches(searchTerm: HTMLInputElement) {
+    if (searchTerm.value === '') {
+      this.firebase.database()
+        .ref('people')
+        .orderByChild('isCoach')
+        .equalTo(true)
+        .on('value', res => {
+          const data = res.val();
+          this.coaches = Object.keys(data).map(key => {
+            const coach = data[key];
+            coach.id = key;
+            coach.type = 'COACH';
+            coach.pbcId = this.personId;
+            return coach;
+          });
+        });
+    }
+    this.coaches = this.coaches.filter(coach => {
+      if (coach.firstName.includes(searchTerm.value)) {
+        return coach;
+      }
+    });
+  }
 
 }
